2f2f89d072e0982ba2767ba19895598cb7d8c27b
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2017-10-16 21:32:47 -0200

    M1#A13

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
+import Title from './title'
+import Square from './square'
 
-const App = React.createClass({
-  render: function () {
-    return <h1>Aplicação</h1>
+// const App = React.createClass({
+//   render: function () {
+//     return <h1>Aplicação</h1>
+//   }
+// })
+
+class App extends Component {
+  render () {
+    return (
+      <div className='container'>
+        <Title name='Fernando' />
+        {['blue', 'red', 'green'].map((square) => (
+          <Square key={square} color={square} />
+        ))}
+      </div>
+    )
   }
-})
+}
 
 export default App
